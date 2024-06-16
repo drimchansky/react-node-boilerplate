c5280f0133b03b87efca405a6837b133cd313534
Nikita Chernov <drimchansky@gmail.com>
2024-06-16 13:56:00 +0400

    add prettier-plugin-tailwindcss

--- client/src/features/counter/ui/counter.tsx
+++ client/src/features/counter/ui/counter.tsx
@@ -6,7 +6,7 @@ export const Counter = () => {
   const counterValue = useStore($counter)
 
   return (
-    <div className="flex flex-col gap-1 items-center">
+    <div className="flex flex-col items-center gap-1">
       <span className="text-lg leading-8 text-gray-600">{counterValue}</span>
       <button
         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- client/src/widgets/header/ui/header.tsx
+++ client/src/widgets/header/ui/header.tsx
@@ -2,7 +2,7 @@ import { Link } from '@tanstack/react-router'
 
 export const Header = () => {
   return (
-    <div className="p-2 flex gap-2">
+    <div className="flex gap-2 p-2">
       <Link className="[&.active]:font-bold" to="/">
         Home
       </Link>
